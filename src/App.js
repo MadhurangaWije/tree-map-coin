ae3ef7377f6c365c5987f7dfca2e96477ec3f3dd
charith <charith9422@gmail.com>
2021-07-04 00:29:04 +0530

    added ui changes for filter

--- src/App.js
+++ src/App.js
@@ -5,13 +5,26 @@ import "react-d3-treemap/dist/react.d3.treemap.css";
 import Dashboard from './components/Dashboard';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormControl from '@material-ui/core/FormControl';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+}));
 
 function App() {
   const [data, setData] = useState(null);
   const [category, setCategory] = useState("markets");
   const [blockSize, setBlockSize] = useState("market_cap");
   //const [currency, setCurrency] = useState(null);
-
+  const classes = useStyles();
   useEffect(() => {
     axios.get(`https://api.coingecko.com/api/v3/coins/${category}?vs_currency=USD&order=market_cap_desc&per_page=20&page=1`)
       .then(res => {
@@ -55,23 +68,37 @@ function App() {
     <div className="App">
       <h2>Crypto Graph</h2>
       <div className="card card-header">
-        <Select
-          disableUnderline
-          value={category}
-          onChange={(e) => setCategoryType(e.target.value)}
-        >
-          <MenuItem value="markets">Market</MenuItem>
-          <MenuItem value="categories">Coin Category</MenuItem>
-        </Select>
-        <Select
-          disableUnderline
-          value={blockSize}
-          onChange={(e) => setBlockSizeType(e.target.value)}
-        >
-          <MenuItem value="market_cap">Market CAP</MenuItem>
-          <MenuItem value="total_supply">Total Supply</MenuItem>
-          <MenuItem value="total_volume">Total Volume</MenuItem>
-        </Select>
+
+        <div>
+          <FormControl className={classes.formControl}>
+            <InputLabel id="select-category">Category</InputLabel>
+            <Select
+              labelId="select-category"
+              disableUnderline
+              value={category}
+              onChange={(e) => setCategoryType(e.target.value)}
+            >
+              <MenuItem value="markets">Market</MenuItem>
+              <MenuItem value="categories">Coin Category</MenuItem>
+            </Select>
+          </FormControl>
+        </div>
+
+        <div>
+          <FormControl className={classes.formControl}>
+            <InputLabel id="select-blocksize">BlockSize By</InputLabel>
+            <Select
+              labelId="select-blocksize"
+              disableUnderline
+              value={blockSize}
+              onChange={(e) => setBlockSizeType(e.target.value)}
+            >
+              <MenuItem value="market_cap">Market CAP</MenuItem>
+              <MenuItem value="total_supply">Total Supply</MenuItem>
+              <MenuItem value="total_volume">Total Volume</MenuItem>
+            </Select>
+          </FormControl>
+        </div>
         {/* <Select
           disableUnderline
           value="Currency"
